b21b74c1db4677624671f7b72efab2b2b14db261
Jeremy23a <jeremy.andre.99@gmail.com>
2023-04-29 14:00:23 -0500

    s

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <Box>
+    <Box position='fixed'>
       <Flex
         display={{ base: "none", md: "flex" }}
         direction="row"
